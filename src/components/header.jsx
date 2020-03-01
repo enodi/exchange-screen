4b713cbecbfe73e352cbb6bd0304462700e3418d
Enodi Audu <enodiaudu5@gmail.com>
2020-03-01 14:52:00 +0100

    design header component

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "images/bar.svg";
+
+export const Header = () => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <NavLink className="header__title" to="/">
+          <h1>Revolut</h1>
+          <img src={logo} className="logo" alt="logo" />
+        </NavLink>
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
